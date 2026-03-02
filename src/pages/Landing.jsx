38d17ee393667b1dcc64fac592c89d5d26b340bc
David Hernandez Asaf <d.hernandeza2@uniandes.edu.co>
2026-03-02 10:34:41 -0500

    Arreglo

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,12 +1,14 @@
-import HeroSection from "@/components/landing/HeroSection";
-import HowItWorks from "@/components/landing/HowItWorks";
-import Footer from "@/components/landing/Footer";
-import Navbar from "@/components/landing/Navbar";
+import TopBar from "@/components/TopBar";
+import MainNav from "@/components/MainNav";
+import HeroSection from "@/components/HeroSection";
+import HowItWorks from "@/components/HowItWorks";
+import Footer from "@/components/Footer";
 
 const Index = () => {
   return (
     <div className="min-h-screen flex flex-col bg-background">
-      <Navbar />
+      <TopBar />
+      <MainNav />
       <main className="flex-1">
         <HeroSection />
         <HowItWorks />
