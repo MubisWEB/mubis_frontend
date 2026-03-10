73b8b99c7d3a696065b18a29eedd73bec43f2731
David Hernandez Asaf <d.hernandeza2@uniandes.edu.co>
2026-03-09 20:07:38 -0500

    logo en la pagina web

--- src/components/MubisLogo.jsx
+++ src/components/MubisLogo.jsx
@@ -10,10 +10,15 @@ export default function MubisLogo({ size = 'md', linkTo = null }) {
   };
 
   const logo = (
-    <span className={`${sizes[size]} font-black tracking-tight text-foreground`}>
-      mub<span className="relative">i<span className="absolute left-1/2 -translate-x-1/2 -top-[0.08em] w-[0.28em] h-[0.28em] rounded-full bg-primary" /></span>s
+  <span className={`${sizes[size]} font-black tracking-tight text-foreground`}>
+    mub
+    <span className="relative">
+      i
+      <span className="absolute left-1/2 -translate-x-1/2 -top-[-0.17em] w-[0.22em] h-[0.19em] rounded-full bg-primary" />
     </span>
-  );
+    s
+  </span>
+);
 
   if (linkTo) {
     return <Link to={linkTo}>{logo}</Link>;
